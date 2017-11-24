e7cf42d638e8db304196326d623f42315275740e
wdn007 <154934119@qq.com>
2017-11-24 11:33:17 +0800

    添加文章页面两个

---
+++ src/cn/guet/mooc/dao/ArticleDAO.java
@@ -0,0 +1,37 @@
+package cn.guet.mooc.dao;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.stereotype.Repository;
+
+import cn.guet.mooc.entity.ArticleEntity;
+
+import javax.annotation.Resource;
+
+import java.util.List;
+
+/**
+ * Created by wdn
+ */
+@Repository // 用于将数据访问层 (DAO 层 ) 的类标识为 Spring Bean
+public class ArticleDAO
+{
+    @Resource
+    private SessionFactory sessionFactory;
+    private Session getSession() {
+        return sessionFactory.getCurrentSession();
+    }
+
+    public Long count(String hql) {
+        // Query q = getCurrentSession().createQuery(hql).;
+        // return (Long) q.uniqueResult();
+        return (Long) sessionFactory.getCurrentSession().createQuery(hql).list().get(0);
+    }
+
+    public List<ArticleEntity> find(String hql, int page, int rows) {
+        Query q = sessionFactory.getCurrentSession().createQuery(hql);
+        return q.setFirstResult((page - 1) * rows).setMaxResults(rows).list();
+    }
+
+}
